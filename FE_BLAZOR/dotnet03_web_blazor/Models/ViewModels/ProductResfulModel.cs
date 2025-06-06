6a11f07f9777d9c4da57397ab15e0a2e9e074f67
khoi <khoimap2307@gmail.com>
2025-06-06 20:14:22 +0700

    resfulapi

---
+++ FE_BLAZOR/dotnet03_web_blazor/Models/ViewModels/ProductResfulModel.cs
@@ -0,0 +1,10 @@
+public class ProductResfulModel
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public string price { get; set; }
+    public string img { get; set; }
+    public string description { get; set; }
+    public string type { get; set; }
+    public bool deleted { get; set; }
+}
